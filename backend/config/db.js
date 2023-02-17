e817ddf97125a6dceac1d53a17d2dcdf605eff3c
Luís Massuchini <luis.massuchini@gmail.com>
2023-02-17 13:53:36 -0300

    Iniciando Projeto

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPasswod = process.env.DB_PASS;
+
+const conn = async () => {
+  try {
+    const dbConn = await mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPasswod}@cluster0.ix5jiiw.mongodb.net/?retryWrites=true&w=majority`
+    );
+
+    console.log("Conectou ao banco!");
+
+    return dbConn;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+conn()
+
+module.exports = conn;
