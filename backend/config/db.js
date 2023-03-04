0327a169e3ee235f157b1d5ddd82bd714f1dc5a6
Luís Massuchini <luis.massuchini@gmail.com>
2023-03-04 14:38:08 -0300

    Finalizando o backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,7 @@ const dbUser = process.env.DB_USER;
 const dbPasswod = process.env.DB_PASS;
 
 const conn = async () => {
+  mongoose.set("strictQuery", false);
   try {
     const dbConn = await mongoose.connect(
       `mongodb+srv://${dbUser}:${dbPasswod}@cluster0.ix5jiiw.mongodb.net/?retryWrites=true&w=majority`
--- backend/controllers/UseController.js
+++ backend/controllers/UseController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { default: mongoose } = require("mongoose");
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -14,10 +15,139 @@ const generateToken = (id) => {
 
 // Register user and sign in
 const register = async (req, res) => {
-  res.send("Registro");
+  const { name, email, password } = req.body;
+
+  //check if user exits
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(422).json({ erros: ["Email já cadastrado"] });
+    return;
+  }
+
+  // Generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // Create user
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  // If user was created successfully, return the token
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ erros: ["Houve um erro, por favor tente mais tarde."] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
+};
+
+//Sing user in
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  //Check if user  exists
+  if (!user) {
+    res.status(404).json({ erros: ["Usuário não encontrado."] });
+    return;
+  }
+
+  //Check if password matches
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({ erros: ["Senha inválida."] });
+    return;
+  }
+
+  //Return user with token
+  res.status(201).json({
+    _id: user._id,
+    profileImage: user.profileTmage,
+    token: generateToken(user._id),
+  });
+};
+
+//Get current logged in user
+const getCurrentUser = async (req, res) => {
+  const user = req.user;
+
+  res.status(200).json(user);
+};
+
+//Update an user
+const update = async (req, res) => {
+  const { name, password, bio } = req.body;
+
+  let profileImage = null;
+
+  if (req.file) {
+    profileImage = req.file.filename;
+  }
+
+  const reqUser = req.user;
+
+  const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select(
+    "-password"
+  );
+
+  if (name) {
+    user.name = name;
+  }
+  if (password) {
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    user.password = passwordHash;
+  }
+
+  if (profileImage) {
+    user.profileTmage = profileImage;
+  }
+
+  if (bio) {
+    user.bio = bio;
+  }
+
+  await user.save();
+
+  res.status(200).json(user);
+};
+
+//Get user by id
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(mongoose.Types.ObjectId(id)).select(
+      "-password"
+    );
+
+    //check if user exits
+    if (!user) {
+      res.status(404).json({ errors: ["Usuário não encontrado."] });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
 };
 
 module.exports = {
-    register,
-    
-}
+  register,
+  login,
+  getCurrentUser,
+  update,
+  getUserById,
+};
