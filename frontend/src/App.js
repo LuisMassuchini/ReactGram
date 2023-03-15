9c602f278ab89e1a032f3dd436ab52fea2df1848
Luís Massuchini <luis.massuchini@gmail.com>
2023-03-15 15:47:37 -0300

    Iniciando as configurações do Redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,28 @@
-import "./Auth.css";
+import "./App.css";
 //Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import "./App.css"
 
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
